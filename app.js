5a33cb2dacfaded1e9bc53dc7a9f09545f77b92d
toshiyanishimura1963 <toshiya.nishimura@gmail.com>
2019-02-06 18:18:16 +0900

    20190206a

--- app.js
+++ app.js
@@ -27,13 +27,6 @@ function renderCafe(doc) {
     })
 }
 
-// getting data
-// db.collection('cafes').get().then((snapshot) => {
-//     snapshot.docs.forEach(doc => {
-//         renderCafe(doc);
-//     })
-// })
-
 // saving data
 form.addEventListener('submit', (e) => {
     e.preventDefault();
